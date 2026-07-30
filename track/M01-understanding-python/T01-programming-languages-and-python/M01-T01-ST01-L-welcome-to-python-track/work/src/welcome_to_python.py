d3aa4215cf066425c3b37c67fa58cacd92fb8cdd
Vaishnavi <vaishnavichitragar@gmail.com>
2026-07-30 15:46:40 +0530

    Day1Arithmetic

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
@@ -1,2 +1,16 @@
 print ("hellow world")
 print ("Welcome to KodNest future Developers")
+a=60
+b=90
+sum=a+b
+print("sum",sum)
+sub=a-b
+print("sub",sub)
+mul=a*b
+print("mul",mul)
+div=a/b
+print("div",div)
+per=a%b
+print("per",per)
+input=("enter the value of a")
+input=("enter the value of b") 
