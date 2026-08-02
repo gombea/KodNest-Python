00481da92c352fae84fd143468dfdac0d5f177e4
Vaishnavi <vaishnavichitragar@gmail.com>
2026-08-02 21:02:31 +0530

    pricetotal

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/foundationbas.py
@@ -0,0 +1,11 @@
+rice_price = 35
+rice_qty = 4
+rice_cost = rice_price * rice_qty
+
+juice_price = 20
+juice_qty = 2
+juice_cost = juice_price * juice_qty
+
+bill_total = rice_cost + juice_cost
+
+print(bill_total)
