ff71ca689d75bcf6ad1e4c85d23f9bd6dbffeb50
Vaishnavi <vaishnavichitragar@gmail.com>
2026-08-05 15:56:42 +0530

    condition statements

---
+++ track/M02-introduction-to-python/T01-python and basic constructs/src/addtwonumbers.py
@@ -0,0 +1,5 @@
+first_number = int(input())
+second_number = int(input())
+
+sum_result=first_number+second_number
+print(f"Sum:{sum_result}")
---
+++ track/M02-introduction-to-python/t02-condition/src/nestedif.py
@@ -0,0 +1,11 @@
+username="vaish"
+password="vaish@234"
+
+if username == "vaish":
+    print("Username is correct")
+    if password == "vaish@234":
+        print("Login successful")
+    else:
+        print("Invalid password")
+else:
+    print("Invalid username")
