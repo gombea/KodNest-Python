0b40b87642ac284f3938aeb077191f058d2c5455
Vaishnavi <vaishnavichitragar@gmail.com>
2026-08-07 16:19:58 +0530

    unpacking

---
+++ track/M02-introduction-to-python/T03-List,Tuple,sets,dictionary/src/unpackingtuple.py
@@ -0,0 +1,11 @@
+name = input()
+course = input()
+score = int(input())
+
+student_record = (name,course,score)
+a,b,c = student_record
+
+print("Name",a)
+print("Course",b)
+print("Score",c)
+# name,course,_ = student_record
